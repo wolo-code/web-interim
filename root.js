63d270ba04afc376751fd221414c41b8905b5416
Ujjwal Singh <ujjwalsingh@outlook.com>
2020-06-25 04:09:55 +0530

    published

--- root.js
+++ root.js
@@ -2167,11 +2167,15 @@ function noCityWait_stop() {
 	notification_top.classList.remove('hide');
 }
 function hideOverlay(e) {
+	var visible_div;
 	for(var child= document.getElementById('overlay').childNodes[1].firstChild; child!==null; child=child.nextSibling)
-		if(child.nodeType == 1 && !child.classList.contains('hide'))
+		if(child.nodeType == 1 && !child.classList.contains('hide')) {
+			visible_div = child;
 			child.classList.add('hide');
+		}
 	if(!document.getElementById('overlay').classList.contains('hide'))
-		document.getElementById('overlay').classList.add('hide');			
+		if(visible_div == e)
+			document.getElementById('overlay').classList.add('hide');			
 }
 
 function showOverlay(e) {
